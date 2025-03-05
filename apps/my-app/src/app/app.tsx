e8f82cd60dd316ffe0d5bed1d5791d01c0e30554
Vaclav Prokes <vaclav.prokes@epcommodities.cz>
2025-03-05 02:48:45 +0100

    shadcn ui

--- apps/my-app/src/app/app.tsx
+++ apps/my-app/src/app/app.tsx
@@ -7,12 +7,13 @@ export function App() {
     <section className="bg-white dark:bg-gray-900">
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
         <Hero title="Welcome @workspace/my-app">
-          <Button size="sm">Button</Button>
+          <Button size="sm" variant="destructive">
+            Button
+          </Button>
         </Hero>
         <div className="flex items-center justify-center min-h-svh">
           <div className="flex flex-col items-center justify-center gap-4">
             <h1 className="text-2xl font-bold">How its going?</h1>
-           
           </div>
         </div>
       </div>
