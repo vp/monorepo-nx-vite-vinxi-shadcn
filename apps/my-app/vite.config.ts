ee1c65f4150b0fc6a8f8d1627db5c310bcfcecf1
Vaclav Prokes <vaclav.prokes@epcommodities.cz>
2025-03-07 01:46:56 +0100

    shadcn ui basic integration

--- apps/my-app/vite.config.ts
+++ apps/my-app/vite.config.ts
@@ -1,6 +1,8 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from "path";
 
 export default defineConfig({
   root: __dirname,
@@ -13,7 +15,12 @@ export default defineConfig({
     port: 4300,
     host: 'localhost',
   },
-  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [react(), tailwindcss()],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
