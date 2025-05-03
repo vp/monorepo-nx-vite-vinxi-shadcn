e3ab48a521b7f346f5e0260ccb79eb99c1e148e2
Vaclav Prokes <vaclav.prokes@epcommodities.cz>
2025-05-03 21:05:24 +0200

    fiix config

--- packages/todos-supabase/eslint.config.mjs
+++ packages/todos-supabase/eslint.config.mjs
@@ -1,3 +1,12 @@
 import eslint from '@workspace/eslint-config';
 
-export default [...eslint.configs.base, ...eslint.configs.lib];
+export default [
+    ...eslint.configs.base,
+    {
+      files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+      // Override or add rules here
+      rules: {
+        "@nx/enforce-module-boundaries": ["off"]
+      },
+    },
+  ];
