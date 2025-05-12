94cfa11e456021834e2b1b6400df4239adef3870
Vaclav Prokes <vaclav.prokes@mouseover.cz>
2025-05-12 12:40:07 +0200

    feat(trpc): refactor header handling for TRPC client and server integration

--- packages/trpc/src/client.ts
+++ packages/trpc/src/client.ts
@@ -2,6 +2,8 @@ import {
   createTRPCClient as createTRPCClientBase,
   HTTPBatchLinkOptions,
   httpBatchStreamLink,
+  httpSubscriptionLink,
+  splitLink,
 } from '@trpc/client';
 import superjson from 'superjson';
 import { AnyTRPCRouter } from '@trpc/server';
@@ -10,11 +12,25 @@ import { getTrpcServerHeaders } from './trcp-server-headers.js';
 export const createTRPCClient = <TRouter extends AnyTRPCRouter>() =>
   createTRPCClientBase<TRouter>({
     links: [
-      httpBatchStreamLink({
-        transformer: superjson,
-        url: getUrl(),
-        headers: getTrpcServerHeaders,
-      } as unknown as HTTPBatchLinkOptions<TRouter['_def']['_config']['$types']>),
+      splitLink({
+        // Use HTTP subscription link for subscription operations
+        condition: (op) => op.type === 'subscription',
+
+        // HTTP subscription link for subscriptions (long-polling)
+        true: httpSubscriptionLink({
+          url: getUrl(),
+          // Customize as needed for subscriptions
+          transformer: superjson,
+          headers: getTrpcServerHeaders,
+        }),
+
+        // HTTP batch stream link for queries and mutations
+        false: httpBatchStreamLink({
+          transformer: superjson,
+          url: getUrl(),
+          headers: getTrpcServerHeaders,
+        } as unknown as HTTPBatchLinkOptions<TRouter['_def']['_config']['$types']>),
+      }),
     ],
   });
 
